2577122ea669cb18ada614e78f693297d44a4df3
PabloFerreirax <pablo.estudoss@gmail.com>
2020-08-03 07:36:46 -0300

    Primeira Entidade User adicionada e resource

---
+++ src/main/java/com/example/application/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.application.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.application.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Pablo", "pablo@pablo.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
