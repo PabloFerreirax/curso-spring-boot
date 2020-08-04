acc4a7cfbc3170e25ca236671963d09abb584085
PabloFerreirax <pablo.estudoss@gmail.com>
2020-08-03 22:58:54 -0300

    Adicionado metodo de inserção

--- src/main/java/com/example/application/resources/UserResource.java
+++ src/main/java/com/example/application/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.example.application.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.application.entities.User;
 import com.example.application.services.UserService;
@@ -29,5 +33,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){ // esta @ anotação serve para dizer que vamos utilizar o id declarado acima
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping // usado para inserir um novo recurso no banco
+	public ResponseEntity<User> insert(@RequestBody User obj){ // esta anotaçao diz que quando chegar no mode json e esse json vai ser descerializado para um obj user
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}") // .path("/{id}") recebe um padrao para montar url... colocar aquilo que colocou em FindById
+				.buildAndExpand(obj.getId()).toUri(); // .buildAndExpand(obj.getId()) espera o Id do que foi inserido... precisa pegar o id /// toUri converte.
+		return ResponseEntity.created(uri).body(obj); //created espera um URI acima digo como fazer ela
+		// esta tag é muito util para criação do 201 que retorna tudo sobre o obj criado.
 	}
 }
